86effcb1b78165bae19067e51ec240a4cb1610a5
Shintaro Matsushima <shintaromatsushima@gmail.com>
2018-09-04 21:10:10 +0900

    Bring back helm layer

--- init.el
+++ init.el
@@ -45,7 +45,8 @@ values."
                       auto-completion-enable-help-tooltip t
                       auto-completion-enable-snippets-in-popup t
                       auto-completion-enable-sort-by-usage t)
-     ivy
+     helm
+     
      ;; 5 Emacs
      better-defaults
 
